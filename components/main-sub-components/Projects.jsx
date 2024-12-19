b2e5d94e76a5ebbcf07453973c7ecccaa82d6983
Firerr <steelejames022@gmail.com>
2024-12-19 19:07:16 +0000

    imported assets

--- components/main-sub-components/Projects.jsx
+++ components/main-sub-components/Projects.jsx
@@ -1,20 +1,26 @@
 import { portfolioList } from '@/data/portfolioList.js';
-import placeHolder from '@/assets/Placeholder.png'
+import placeHolder from '@/assets/Placeholder.png';
 import Image from 'next/image';
+
 
 const Projects = () => {
   console.log(placeHolder);
   return (
     <>
       <h2>Projects</h2>
-      <ul className='portfolioList'>
+      <ul className="portfolioList">
         {portfolioList.map((e, i) => (
-          <a href= {e.url}>
-          <li className="portfolioItem" key={i}>
-            <h3>{e.name}</h3>
-            <Image src={e.projectImage || placeHolder} width={200} height={200} alt={e.name} />
-            <p>{e.description}</p>
-          </li>
+          <a href={e.url} key={i}>
+            <li className="portfolioItem">
+              <h3>{e.name}</h3>
+              <Image
+                src={e.projectImage || placeHolder}
+                width={200}
+                height={200}
+                alt={e.name}
+              />
+              <p>{e.description}</p>
+            </li>
           </a>
         ))}
       </ul>
--- data/portfolioList.js
+++ data/portfolioList.js
@@ -1,32 +1,37 @@
+import weatherImage from '/assets/five-day-forecast/main.png';
+import christmasImage from '/assets/christmas-movie-picker/main.png';
+import fourCardImage from '/assets/fourcard/main.png';
+import catImage from '/assets/catsapp/main.png';
+
 export const portfolioList = [
   {
     name: '5 Day Forecast',
     description: 'A 5 day forecast made with Vite and OpenWeather API',
-    projectImage: '',
-    url: '',
-    gitLink: '',
+    projectImage: weatherImage,
+    url: 'https://5-day-forecast.vercel.app/',
+    gitLink: 'https://github.com/Firerr/5-day-forecast',
   },
   {
     name: 'Christmas Movie Picker',
     description:
       'A day group project, that picks a Christmas movie from themoviedb.org based on a selection of choices, uses an API, vite and tailwind',
-    projectImage: '',
-    url: '',
-    gitLink: '',
+    projectImage: christmasImage,
+    url: 'https://christmas-movie-picker.vercel.app/',
+    gitLink: 'https://github.com/Firerr/christmas-movie-picker',
   },
   {
     name: 'Four Card Feature',
     description:
       'Very early in learning code I had to use make the site look as close to the target as possible, uses html and css',
-    projectImage: '',
-    url: '',
-    gitLink: '',
+    projectImage: fourCardImage,
+    url: 'https://four-card-feature-sigma-one.vercel.app/',
+    gitLink: 'https://github.com/Firerr/four-card-feature',
   },
   {
     name: 'Cats Main',
     description: 'uses postman and vite',
-    projectImage: '',
-    url: '',
-    gitLink: '',
+    projectImage: catImage,
+    url: 'https://cats-main.vercel.app/',
+    gitLink: 'https://github.com/Firerr/cats-main',
   },
 ];
